63c44fae6d1ea5b06a768bb43e932c6c8b995a80
Pentesting-Lab <62560278+Pentesting-Lab@users.noreply.github.com>
2020-03-25 17:48:39 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,30 @@
+clear
+clear
+echo -e "  
+    ╦ ╦┌─┐┌─┐┬┌─┌─┐┌─┐┬  ┌─┐┬┌┬┐
+    ╠═╣├─┤│  ├┴┐└─┐├─┘│  │ ││ │  (Linux) 
+    ╩ ╩┴ ┴└─┘┴ ┴└─┘┴  ┴─┘└─┘┴ ┴  (By Pratik (My Github account: Pentesting-Lab))
+
+
+
+                                     Installer
+
+                  Note: Hacksploit is a tool for making Metasploit easier and faster. 
+                 This tool automates Metasploit. THIS IS ONLY FOR EDUCATIONAL PURPOSE!
+
+"
+
+echo -ne 'Installing on your system...\r'
+cp Hacksploit /usr/bin/hacksploit
+sleep 2
+echo -e "\e[1;32m
+
+         Hacksploit Has been Installed on your system. For trying it type 'hacksploit'on Terminal.
+
+
+
+\e[0m "
+
+
+
+
